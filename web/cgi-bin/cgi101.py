62d461b491d593a9f5755a56cbe4189c37627e1d
gavinpham <gpham@smu.edu>
2016-06-27 13:55:59 +0900

    Added file uploading

--- web/cgi-bin/cgi101.py
+++ web/cgi-bin/cgi101.py
@@ -1,9 +1,41 @@
 #!/usr/bin/env python
-import cgi
+import cgi, os
+import cgitb; cgitb.enable()
+
+try: # Windows needs stdio set for binary mode. IDK IF THIS IS NEEDED OR NOT
+    import msvcrt
+    msvcrt.setmode (0, os.O_BINARY) # stdin  = 0
+    msvcrt.setmode (1, os.O_BINARY) # stdout = 1
+except ImportError:
+    pass
+
 form = cgi.FieldStorage()                 # parse form data
-print('Content-type: text/html\n')        # hdr plus blank line
-print('<title>Reply Page</title>')        # html reply page
-if not 'user' in form:
-    print('<h1>Who are you?</h1>')
+fileitem = form['file']
+
+# Test if the file was uploaded
+if fileitem.filename:
+
+    # strip leading path from file name
+    # to avoid directory traversal attacks
+    fn = os.path.basename(fileitem.filename)
+    open('./files/' + fn, 'wb').write(fileitem.file.read())
+    message = 'The file "' + fn + '" was uploaded successfully'
+
 else:
-    print('<h1>Hello <i>%s</i>!</h1>' % cgi.escape(form['user'].value))
+	message = 'No file was uploaded'
+
+print """\
+Content-Type: text/html\n
+<html><body>
+<p>%s</p>
+</body></html>
+""" % (message,)
+
+
+# print('Content-type: text/html\n')        # hdr plus blank line
+# print('<title>Reply Page</title>')        # html reply page
+# if not 'user' in form:
+#     print('<h1>Who are you?</h1>')
+# else:
+#     print('<h1>Hello <i>%s</i>!</h1>' % cgi.escape(form['user'].value))
+
