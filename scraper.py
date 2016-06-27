1a39a9e90b188122959dff40878309ab42822205
gavinpham <gpham@smu.edu>
2016-06-27 22:19:39 +0900

    SO many things

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
 import os
-os.system("image-scraper  http://www.prettydesigns.com/70-cool-korean-japanese-hairstyles-asian-guys-2015/")
+# os.system("image-scraper  http://www.prettydesigns.com/70-cool-korean-japanese-hairstyles-asian-guys-2015/")
 
-for file in os.listdir("/Users/Gavin/facecheck/images_www.prettydesigns.com"):
+for file in os.listdir("/Users/Gavin/facecheck/web/seed_imgs"):
     if file.endswith(".jpg"):
         print(file)
         file_path = "./images_www.prettydesigns.com/" + str(file)
 
         os.system("python ./faceswap.py " + file_path + " ira_2.jpg " + file)
+
+form = cgi.FieldStorage()                 # parse form data
---
+++ web/perpetual_swapper.py
@@ -0,0 +1,21 @@
+import os, time
+
+while(1):
+	for face_img in os.listdir("/Users/Gavin/facecheck/web/files"):
+		if face_img.endswith(".jpg"):
+			print("Image found: " + face_img)
+			face_img_path = "./files/" + face_img
+			for file in os.listdir("/Users/Gavin/facecheck/web/seed_imgs"):
+			    if file.endswith(".jpg"):
+			        print("\tSwapping faces: " + file)
+			        file_path = "./seed_imgs/" + str(file)
+			        output = file
+
+			        print (face_img_path)
+			        print (file_path)
+			        os_call = "python ./faceswap.py " + file_path + " " + face_img_path + " " + output
+			        print os_call
+			        os.system(os_call)
+
+			os.system("rm /Users/Gavin/facecheck/web/files/" + .jpg")
+	time.sleep(100)
